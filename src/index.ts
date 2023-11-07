9fec11ee7a3c533b3e62399628e9fbe98632b513
Alexey Kotlyarov <a@koterpillar.com>
2023-11-07 21:36:46 +1100

    mergeNodes

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,51 @@
-import Parser, { SyntaxNode } from 'tree-sitter';
+import Parser, { SyntaxNode } from "tree-sitter";
 
-import JSON from 'tree-sitter-json';
+import JSON from "tree-sitter-json";
 
-type Language = 'json';
+type Language = "json";
 
 const languages: Record<Language, any> = {
   json: JSON,
 };
 
 function prettyPrint(node: SyntaxNode, level = 0): void {
-  const indent = '  '.repeat(level);
-  if (node.isNamed) {
-    console.log(indent, node.type, node.text.replace(/\n/g, '\\n'));
+  const indent = "  ".repeat(level);
+  if (!node.isNamed) {
+    console.log(indent, node.type);
+  } else if (node.type === "string" || node.type === "number") {
+    console.log(indent, node.text.replace(/\n/g, "\\n"));
   } else {
     console.log(indent, node.type);
-  }
-  for (const child of node.children) {
-    prettyPrint(child, level + 1);
+    for (const child of node.children) {
+      prettyPrint(child, level + 1);
+    }
   }
 }
 
-export function merge(language: Language, base: string, left: string, right: string): string {
+export function mergeNodes(
+  language: Language,
+  base: SyntaxNode,
+  left: SyntaxNode,
+  right: SyntaxNode
+): SyntaxNode {
+  prettyPrint(base);
+  throw new Error("Not implemented");
+}
+
+export function merge(
+  language: Language,
+  base: string,
+  left: string,
+  right: string
+): string {
   const parser = new Parser();
   parser.setLanguage(languages[language]);
 
-  const baseTree = parser.parse(base);
+  const baseNode = parser.parse(base).rootNode;
+  const leftNode = parser.parse(left).rootNode;
+  const rightNode = parser.parse(right).rootNode;
 
-  prettyPrint(baseTree.rootNode);
+  const resultNode = mergeNodes(language, baseNode, leftNode, rightNode);
 
-  throw new Error('Not implemented');
+  return resultNode.toString();
 }
